ad48141e6a5eec939c40d4cb458433e18f494f05
Andrei800 <vedernikov485@gmail.com>
2021-11-21 13:30:27 +0100

    first commit

---
+++ yatube/posts/views.py
@@ -0,0 +1,33 @@
+from django.db.models.fields import SlugField
+from django.shortcuts import get_object_or_404, render
+from .models import Post, Group
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    title = 'Список групп'
+    text = 'Информация о группах проекта Yatube'
+    context = {
+        "title" : title,
+        "text" : text,
+    }
+    return render(request, template, context)
+
+# Страница с постами отфильтрованные по группам
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug = slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
