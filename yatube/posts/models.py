3cd581505de88b69ca1ea9532bcfabfcbe6414a0
Andrei800 <92823970+Andrei800@users.noreply.github.com>
2021-11-24 07:00:30 +0100

    Update models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -34,5 +34,5 @@ class Post(models.Model):
     class Meta:
         ordering = ('-pub_date',)
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.text[:15]
